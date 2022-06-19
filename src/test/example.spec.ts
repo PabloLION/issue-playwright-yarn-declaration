6655f2b3e58ac94b663f48829a27a0fd237bde82
Pablo LION <36828324+Pablion@users.noreply.github.com>
2022-06-20 01:19:30 +0200

    test: rm redundant test

--- src/test/example.spec.ts
+++ src/test/example.spec.ts
@@ -5,9 +5,3 @@ test("basic test", async ({ page }) => {
   const title = page.locator(".navbar__inner .navbar__title");
   await expect(title).toHaveText("Playwright");
 });
-
-test('log "something"', () => {
-  // eslint-disable-next-line
-  console.log("something"); // this is better to show call stack
-  // TODO: override logger.info with console.log
-});
